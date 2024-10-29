a7ddd3e5fde1a5d804d34d191f497770a96ebcf1
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-10-29 14:41:43 +0000

    시간초과 해결해보기

--- BOJ/BOJ_23286/Main.java
+++ BOJ/BOJ_23286/Main.java
@@ -3,10 +3,11 @@ import java.io.*;
 import java.lang.Math;
 
 public class Main {
-    public static long INF = Long.MAX_VALUE;
+    public static long INF = 987654321L;
     public static int N, M, T;
     public static long[][] dist;
-    public static int[][] path;
+    public static boolean[] visit;
+    public static long MAX;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,12 +22,10 @@ public class Main {
         T = Integer.parseInt(st.nextToken());
 
         dist = new long[N + 1][N + 1];
-        path = new int[N + 1][N + 1];
+        visit = new boolean[N + 1];
         for(int i = 1; i <= N; i++)
-            for(int j = 1; j <= N; j++) {
-                if(i == j) dist[i][j] = 0;
-                else dist[i][j] = INF;
-            }
+            for(int j = 1; j <= N; j++)
+                dist[i][j] = INF;
         
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -37,33 +36,65 @@ public class Main {
             h = Integer.parseInt(st.nextToken());
 
             dist[u][v] = Math.min(dist[u][v], h);
-            path[u][v] = v;
         }
 
         // Floyd-Warshall
         for(int pass = 1; pass <= N; pass++)
             for(int i = 1; i <= N; i++)
                 for(int j = 1; j <= N; j++) {
-                    if(dist[i][pass] + dist[pass][j] < dist[i][j]) {
-                        dist[i][j] = dist[i][pass] + dist[pass][j];
-                        path[i][j] = path[i][pass];
-                    }
+                    if(dist[i][pass] == INF || dist[pass][j] == INF) continue;
+
+                    dist[i][j] = Math.min(dist[i][j], dist[i][pass] + dist[pass][i]);
                 }
-        
-        // path
+                    
+        // for(int i = 1; i <= N; i++) {
+        //     for(int j = 1; j <= N; j++) {
+        //         if(dist[i][j] == INF) sb.append("0").append(" ");
+        //         else sb.append(dist[i][j]).append(" ");
+        //     }
+        //     sb.append("\n");
+        // }
+
+
+        // dfs
         for(int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
+            MAX = -1L;
 
             int s, e;
             s = Integer.parseInt(st.nextToken());
             e = Integer.parseInt(st.nextToken());
 
-            // 갈 수 없는 경우 스킵
-            if(dist[s][e] == INF || dist[s][e] == 0) { sb.append("-1\n"); continue; }
+            visit[s] = true;
+            dfs(s, e, -1L);
+            visit[s] = false;
 
-            while(s != e) {
-                
-            }
+            // for(int j = 1; j <= N; j++)
+            //     sb.append(visit[j]).append(" ");
+            // sb.append("\n");
+            sb.append(MAX).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    public static void dfs(int cur, int goal, long max) {
+        if(cur == goal) { 
+            if(MAX != -1 && max != -1) MAX = Math.min(MAX, max);
+            else MAX = max;
+
+            return ;
+        }
+
+        for(int to = 1; to <= N; to++) {
+            if(cur == to || visit[to] || dist[cur][to] == INF) continue;
+
+            long nxt_max = Math.max(max, dist[cur][to]);
+            visit[to] = true;
+            dfs(to, goal, nxt_max);
+            visit[to] = false;
         }
     }
 }
